d50fcb3aefe4e4644b26677e34f0f439fdaead5d
kartikey <kartikkey0414@gmail.com>
2025-03-22 17:40:38 +0530

    fs async implemented

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-// require('./modules/os-module');
-// require('./modules/path-module');
+require('./modules/os-module');
+require('./modules/path-module');
 require('./modules/fs-sync-module');
+require('./modules/fs-async-module');
